3f44f435481f9475debcfbcd3ce1c0318a2b2305
DevY <hanys1042@gmail.com>
2022-11-24 14:46:47 +0900

    Adding dist

---
+++ build/_app/immutable/chunks/1-e9dba302.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-73897bd8.js";export{t as component};
---
+++ build/_app/immutable/chunks/2-db2eb0ec.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-0df15927.js";import{default as t}from"../components/pages/_page.svelte-3e02634d.js";export{t as component,r as shared};
