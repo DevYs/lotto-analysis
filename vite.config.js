4cae47e61133b00b7785fc3ec0e68e418272f93c
DevY <hanys1042@gmail.com>
2022-11-24 15:05:32 +0900

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	base: '/lotto-analysis',
+	base: '/lotto-analysis/',
 	plugins: [sveltekit()]
 };
 
